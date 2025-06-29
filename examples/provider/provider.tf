0a06663c4351b6e6a04668389c9450996907d5be
Oliver Binns <mail@oliverbinns.co.uk>
2025-06-29 11:35:42 +0100

    chore: update provider name

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,5 +1,7 @@
+# Copyright (c) HashiCorp, Inc.
+
 provider "appstoreconnect" {
-  issuer_id = "4389f85c-98c6-4023-ab25-8154fcd9460d"
-  key_id = "A1234B5678"
+  issuer_id   = "4389f85c-98c6-4023-ab25-8154fcd9460d"
+  key_id      = "A1234B5678"
   private_key = file("private_api_key.p8")
 }
--- examples/resources/user/resource.tf
+++ examples/resources/user/resource.tf
@@ -1,9 +1,11 @@
+# Copyright (c) HashiCorp, Inc.
+
 resource "appstoreconnect_user" "example" {
   first_name = "Oliver"
-  last_name = "Binns"
+  last_name  = "Binns"
 
   email = "mail@oliverbinns.co.uk"
 
-  all_apps_visible = true
+  all_apps_visible     = true
   provisioning_allowed = true
 }
